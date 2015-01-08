45b20c34a408f419ce2e82226d589a4577dfcff6
Rahul garg <rahul.garg@joshlabs.in>
2015-01-08 16:57:13 +0530

    test rest auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestTest::Application.config.secret_key_base = '37696dc51762661db6191d87ccfd5c896d27f8cb3214ab15f5a83d25756eeb60c519913ffd3a8d0b25e706bb7a13962d9c8cf7b62e622f3571e125b511a17005'
---
+++ db/migrate/20150108105549_create_rest_client.rb
@@ -0,0 +1,13 @@
+class CreateRestClient < ActiveRecord::Migration
+  def change
+    create_table :rest_clients do |t|
+      t.string :name
+      t.text :description
+      t.string :api_key
+      t.string :secret
+      t.boolean :is_master
+      t.boolean :disabled
+      t.timestamps
+    end
+  end
+end
